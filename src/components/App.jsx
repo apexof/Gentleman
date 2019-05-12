0e9676475617d5373252a45b5945a35d96381aa4
Mark Frozen <apexof@gmail.com>
2019-05-12 13:28:39 +0300

    добавил styled-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,19 @@
 import "./App.scss";
+import styled from "styled-components";
 
+const Div = styled.div`
+    background-color: #973d3d;
+    color: #fff;
+    font-size: 30px;
+`;
 function App() {
+    console.log("object");
     return (
-        <div cx="test">
-            апad
+        <div>
+            <Div>styled-components</Div>
+            <div cx="css-modules">
+                Css modules
+            </div>
         </div>
     );
 }
